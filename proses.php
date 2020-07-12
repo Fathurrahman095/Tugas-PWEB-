4ea1026277c3da0683a3dcec858a37cd581a582e
Fathurrahman095 <63111228+Fathurrahman095@users.noreply.github.com>
2020-07-12 13:20:14 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,17 +1,35 @@
-<?php 
-if(isset($_POST['fnama']))
-{
- $fnama = $_POST['fnama'];
- $lalamat = $_POST['lalamat'];
- echo "<span class='success'>Dengan <b>METODE POST</b></span><br/>";
- echo "Nama : ".$fnama."<br/>Alamat : ".$lalamat;
-}
-//--------------------------------------------------------------------
-if(isset($_GET['fnama']))
-{
- $fnama = $_GET['fnama'];
- $lalamat = $_GET['lalamat'];
- echo "<span class='success'>Dengan <b>METODE GET</b></span><br/>";
- echo "Nama : ".$fnama."<br/>Alamat : ".$lalamat;
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div>
+	<?php
+	echo "<head><title> Guest Book</head></title>";
+	$fp = fopen("pendaftaran.txt", "a+");
+
+	$nama =$_POST['nama'];
+	$tempat_lahir =$_POST['tempat_lahir'];
+	$tanggal =$_POST['tanggal'];
+	$alamat =$_POST['alamat'];
+	$kota =$_POST['kota'];
+	$negara =$_POST['negara'];
+	$kode_pos =$_POST['kode_pos'];
+	$hp =$_POST['hp'];
+	$email =$_POST['email'];
+	$tinggi_badan =$_POST['tinggi_badan'];
+	$berat_badan =$_POST['berat_badan'];
+
+	fputs($fp,"$nama|$tempat_lahir|$tanggal|$alamat|$kota|$negara|$kode_pos|$hp|$email|$tinggi_badan|$berat_badan\n");
+	fclose($fp);
+
+	echo "Terima Kasih Atas Partisipasi Anda Mengisi Form Pendaftaran <br>";
+	?>
+	<button class="tombol"><a align="center" href="home.html">Buku Tamu</a></button>
+    <button class="tombol"><a align="center" href="tabel.php">Lihat Pengunjung</a></button>
+	
+
+<div>
+</body>
+</html>
